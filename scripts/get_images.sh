bf3f5145d896e8ed52cceb12f0f1226d7200d83a
theamarks <theamarks@gmail.com>
2024-07-23 12:06:11 -0700

    pull exploreARTIS repo readme and images.

--- scripts/get_images.sh
+++ scripts/get_images.sh
@@ -84,3 +84,45 @@ fi
 cd ..
 rm -rf temp-repo
 
+
+#########################
+# Get images from Seafood-Globalization-Lab/exploreARTIS repo
+# Create a temporary directory for the sparse checkout
+if mkdir temp-repo; then
+    cd temp-repo
+    echo "created temp-repo"
+else
+    echo "Failed to create temp-repo directory. Check permissions."
+    exit 1
+fi
+
+# Initialize a new Git repository
+git init
+
+# Add the remote repository
+git remote add origin https://github.com/Seafood-Globalization-Lab/exploreARTIS.git
+
+# Enable sparse-checkout
+git config core.sparseCheckout true
+
+# Create the sparse-checkout file and define the sparse-checkout path to include only the images directory
+mkdir -p .git/info
+echo "imgs/*" >> .git/info/sparse-checkout
+
+# Pull the main branch from the remote repository
+git pull origin main
+
+# Ensure the destination images directory exists
+mkdir -p ../images
+
+# Move the files from the images directory to your R project directory
+if [ -d "imgs" ] && [ "$(ls -A imgs)" ]; then
+    mv imgs/* ../images/
+    echo "Files moved successfully."
+else
+    echo "No files found in the images directory."
+fi
+
+# Clean up by removing the temporary directory
+cd ..
+rm -rf temp-repo
